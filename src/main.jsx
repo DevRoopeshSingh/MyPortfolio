b79957aa79af4475b271862c8b434569966fd384
DevRoopesh <roopeshsingh993@gmail.com>
2024-10-11 11:17:40 +0530

    changed the portfolio from normal CRA to Vite APP

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error(
+    "Root element not found. Make sure there's a div with id 'root' in your index.html."
+  );
+}
